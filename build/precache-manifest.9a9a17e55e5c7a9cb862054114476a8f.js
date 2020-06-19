e7ed9b1d18040ef541daa91b196de699efcbaa8b
Caio Teixeira <caiovmteixeira@outlook.com>
2020-06-19 17:20:09 -0300

    3.11

---
+++ build/precache-manifest.9a9a17e55e5c7a9cb862054114476a8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95064ca4ef0f4bb30345a583d241360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "776568b707b6b97c6150",
+    "url": "/static/js/2.05a2668f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05a2668f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f4ec14428c976dc907",
+    "url": "/static/js/main.bd7896a5.chunk.js"
+  },
+  {
+    "revision": "b36ae07c9788c9e04d2f",
+    "url": "/static/js/runtime-main.78ccc6d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const cors = require('cors')
 
 
 const app = express()
-app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 
 let persons = [
     {
