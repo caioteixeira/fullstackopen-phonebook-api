aec19dae07ed01be76887bd81986181b363e37d4
Caio Teixeira <caiovmteixeira@outlook.com>
2020-12-16 20:13:52 -0300

    3.15 3.16 3.17

--- index.js
+++ index.js
@@ -12,29 +12,68 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.static('build'))
+
 app.use(express.json())
 
 app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people </p>
+    Person.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people </p>
               <p>${new Date()}\`</p>`)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
-        res.json(persons)
+        res.json(persons.map(person => person.toJSON()))
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+        if(person)
+        {
+            response.json(person.toJSON())
+        }
+        else
+        {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id).then(res => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(res => {
         response.status(204).end()
     })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updated => {
+        response.json(updated.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -52,13 +91,6 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    /*if (persons.find(p => p.name === body.name))
-    {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }*/
-
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -66,7 +98,7 @@ app.post('/api/persons', (request, response) => {
     })
 
     person.save().then(savedPerson => {
-        response.json(savedPerson)
+        response.json(savedPerson.toJSON())
     })
 })
 
@@ -75,6 +107,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
-
+mongoose.set('useFindAndModify', false)
 const url = process.env.MONGODB_URI
 
 console.log('connecting to ', url)
 
 mongoose.connect(url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true})
+    useUnifiedTopology: true})
     .then(result => {
         console.log('connected to MongoDB')})
     .catch((error) => {
